acb668eb0e25b6aefb3c5e814fab087ee38d396d
Ruhi Bhatia <Ruhi Bhatia>
2016-10-04 13:43:50 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Romika Bhatia'});
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', {
+    title: 'About'});
+});
+
+/* GET project page. */
+router.get('/project', function(req, res, next) {
+  res.render('project', {
+    title: 'Project' });
+});
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {
+    title: 'Contact' });
+});
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', {
+    title: 'Services' });
+});
+
+module.exports = router;
